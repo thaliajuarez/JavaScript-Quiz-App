3f79e85e68eac94d848210108c6a1fb1347309ff
Thalía <26156886+thaliajuarez@users.noreply.github.com>
2020-10-27 19:19:02 -0500

    score functionality

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // From HTML document - document object 
-//
+// reserved word used: const
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
+// let - 
 let shuffledQuestions, currentQuestionIndex
-
 // For score counter:
 let countRightAnswers = 0;
 
